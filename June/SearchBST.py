00a33f328dbc221cccb95742ab4de15938d3c510
Sakshi <59663688+sakshi13-m@users.noreply.github.com>
2020-06-16 10:43:50 +0530

    Create SearchBST.py

---
+++ June/SearchBST.py
@@ -0,0 +1,15 @@
+.
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if root.val==val:
+            return root
+        elif root.val>val and root.left!=None:
+            return self.searchBST(root.left,val)
+        elif root.val<val and root.right!=None:
+            return self.searchBST(root.right,val)
